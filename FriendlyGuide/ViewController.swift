193e87675fdd7ce1125c53e1496e7d16947ba14f
Valery Makroguzov <valera.nedbai.developer@gmail.com>
2021-05-23 19:23:38 +0300

    Initial Commit

---
+++ FriendlyGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriendlyGuide
+//
+//  Created by Валерий Макрогузов on 23.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
