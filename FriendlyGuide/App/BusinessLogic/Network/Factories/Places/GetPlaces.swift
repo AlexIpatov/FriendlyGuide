93de7532da8b54aad4167e78e9ae0feac4a4075f
Alexander Pelevinov <alexanderplvv@gmail.com>
2021-06-02 11:58:10 +0300

    getPlacesFactory recreated

--- FriendlyGuide/App/BusinessLogic/Network/Factories/Places/GetPlaces.swift
+++ FriendlyGuide/App/BusinessLogic/Network/Factories/Places/GetPlaces.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class GetPlaces {
+    let sessionManager: URLSession
+    let encoder: ParameterEncoder
+    init(
+        encoder: ParameterEncoder,
+        sessionManager: URLSession) {
+        self.encoder = encoder
+        self.sessionManager = sessionManager
+    }
+}
+
+extension GetPlaces: AbstractRequestFactory {
+    typealias EndPoint = PlacesListResource
+    func request(withCompletion completion:
+                    @escaping (Result<EndPoint.ModelType, NetworkingError>) -> Void) {}
+}
+
+extension GetPlaces: GetPlacesFactory {
+    func getPlaces(cityTag: String,
+                   showingSince: String,
+                   completionHandler: @escaping (Result<EndPoint.ModelType,
+                                                        NetworkingError>) -> Void) {
+        let route = PlacesListResource(cityTag: cityTag, showingSince: showingSince)
+        request(route, withCompletion: completionHandler)
+    }
+}
--- FriendlyGuide/App/BusinessLogic/Network/Factories/Places/GetPlacesFactory.swift
+++ FriendlyGuide/App/BusinessLogic/Network/Factories/Places/GetPlacesFactory.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+protocol GetPlacesFactory {
+    func getPlaces(cityTag: String,
+                   showingSince: String,
+                   completionHandler: @escaping (Result<PlacesList, NetworkingError>) -> Void)
+}
