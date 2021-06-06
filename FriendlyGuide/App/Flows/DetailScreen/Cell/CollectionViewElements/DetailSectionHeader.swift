807e8f1fe6b3465c7d2daf77b64e71eab43898a0
AlexIpatov <65159241+AlexIpatov@users.noreply.github.com>
2021-06-06 20:17:44 +0300

    Detail VC with subviews

---
+++ FriendlyGuide/App/Flows/DetailScreen/Cell/CollectionViewElements/DetailSectionHeader.swift
@@ -0,0 +1,41 @@
+//
+//  DetailSectionHeader.swift
+//  FriendlyGuide
+//
+//  Created by Александр Ипатов on 06.06.2021.
+//
+
+import UIKit
+
+class DetailSectionHeader: UICollectionReusableView {
+    // MARK: - Properties
+    static let reuseId = "DetailSectionHeader"
+    // MARK: - UI components
+    let showMoreButton = UIButton(title: "Показать еще",
+                                                    font: .smallButtonFont(),
+                                                    cornerRadius: 0.0,
+                                                    backgroundColor: .clear,
+                                                    tintColor: .systemBlue)
+
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    //MARK: - Configuration Methods
+    func configureUI() {
+        addSubview(showMoreButton)
+        NSLayoutConstraint.activate([
+            showMoreButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            showMoreButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+            showMoreButton.heightAnchor.constraint(equalToConstant: 20)
+        ])
+
+    }
+}
+
