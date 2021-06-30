d6470a8e10b6327851a48f8d07028716e4dea48b
AlexIpatov <65159241+AlexIpatov@users.noreply.github.com>
2021-06-30 10:22:07 +0300

    Date recovery

---
+++ FriendlyGuide/App/Assembly/Extensions/Date + Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Date + Extension.swift
+//  FriendlyGuide
+//
+//  Created by Александр Ипатов on 30.06.2021.
+//
+
+import Foundation
+
+ extension Date {
+     func goToSimpleDate() -> String {
+         let formatter = DateFormatter()
+         formatter.dateFormat = "dd.MM.yy"
+         return formatter.string(from: self)
+     }
+ }
